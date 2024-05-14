95c456690b74abc25997dc274a259a1008a63027
Denzel Braithwaite <denzelbraithwaite@Denzel-Macbook-Work.local>
2024-05-14 16:06:14 -0400

    trying to add firebase to project for db

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -7,7 +7,7 @@
   export let currentColor: string;
   export let currentItem: string;
 
-  type Square = {
+  type Tile = {
     id: number;
     size?: string;
     color?: string;
@@ -15,21 +15,27 @@
     text?: string;
   }
 
-  let squares: Square[] = [];
+  let squares: Tile[] = [];
   // 2450 / 70 ( col s)
   for (let i = 1; i <= 2450; i++) {
-    squares = [{id: i}, ...squares];
+    squares = [{id: i, color: 'white'}, ...squares];
   }
 
   function sortHandler(e: any): void {
     squares = e.detail.items;
   }
+
+  // function clearMap(): void {
+  //   squares.forEach(sq => sq.color = 'white');
+  //   squares = [...squares];
+  // }
 </script>
 
 <section class="map">
-  {#each squares as s (s.id)}
-  <Square {size} {currentColor} {currentItem}/>
-{/each}
+  {#each squares as sq (sq.id)}
+    <Square {size} {currentColor} {currentItem}/>
+    <!-- <Square {size} {currentColor} {currentItem} square={sq}/> -->
+  {/each}
 </section>
 
 <style lang="scss">
